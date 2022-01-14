5c0a5ada13d8af662bb0dea5f9ddd1ab9153ea65
phongabc334 <phongabc334@gmail.com>
2022-01-14 13:45:14 +0700

    PFP_JAVA_S.L0601

---
+++ PFP_JAVA.S.L0601/src/exercise/ArrayDemo.java
@@ -0,0 +1,24 @@
+/*
+ *
+ *
+ * @author PhongLe
+ * @date Jan 14, 2022
+*/
+package exercise;
+
+public class ArrayDemo {
+	public static void main(String[] args) {
+		
+		int[] arrayInt = new int[5];
+		
+		arrayInt[0] = 1;
+		arrayInt[1] = 2;
+		arrayInt[2] = 3;
+		arrayInt[3] = 4;
+		arrayInt[4] = 5;
+		
+		System.out.println("Array lengh:" + arrayInt.length);
+		
+		System.out.println("Element at index 3 is : " + arrayInt[3]);
+	}
+}
